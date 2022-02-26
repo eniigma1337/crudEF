776365b058ff0f006fc05586e6d94317e1e2330e
anurag <anuragn.0505@gmail.com>
2022-02-26 02:36:14 -0500

    Add project files.

---
+++ CryptopediaWebApp/Controllers/WeaponController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+using WebApplication3.Models;
+using WebApplication3.Models.ViewModels;
+using System.Web.Script.Serialization;
+
+namespace WebApplication3.Controllers
+{
+    public class WeaponController : Controller
+    {
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+
+        static WeaponController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:44311/api/");
+        }     
+        
+        
+        // GET: Weapon/List
+
+        public ActionResult List()
+        {
+            //Objective: Communicate with weapon data api to retrieve a list of weapons
+            //curl https://localhost:44311/api/weapondata/listweapons
+
+            string url = "weapondata/listweapons";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            //Debug.WriteLine("The response is");
+            //Debug.WriteLine(response.StatusCode);
+
+            IEnumerable<WeaponDto> weapons = response.Content.ReadAsAsync<IEnumerable<WeaponDto>>().Result;
+            //Debug.WriteLine("Number of weapons received");
+            //Debug.WriteLine(weapons.Count());
+
+            return View(weapons);
+        }
+
+        // GET: Weapon/Details/5
+        public ActionResult Details(int id)
+        {
+            //Objective: Communicate with weapon data api to retrieve one weapon
+            //curl https://localhost:44311/api/weapondata/findweapon/{id}
+
+            DetailsWeapon ViewModel = new DetailsWeapon();
+
+            string url = "weapondata/findweapon/"+id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            //Debug.WriteLine("The response is");
+            //Debug.WriteLine(response.StatusCode);
+
+            WeaponDto SelectedWeapon = response.Content.ReadAsAsync<WeaponDto>().Result;
+            //Debug.WriteLine("weapon received: ");
+            //Debug.WriteLine(SelectedWeapon.WeaponName);
+            ViewModel.SelectedWeapon = SelectedWeapon;
+
+            return View(ViewModel);
+        }
+
+        public ActionResult Error()
+        {
+
+            return View();
+        }
+
+        // GET: Weapon/New
+        public ActionResult New()
+        {
+            //Information about all collections in the system.
+            //Get api/collectionsdata/listcollections
+            string url = "collectionsdata/listcollections";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            IEnumerable<CollectionsDto> CollectionsOptions = response.Content.ReadAsAsync<IEnumerable<CollectionsDto>>().Result;
+
+            return View(CollectionsOptions);
+        }
+
+        // POST: Weapon/Create
+        [HttpPost]
+        public ActionResult Create(Weapon weapon)
+        {
+            Debug.WriteLine("the jsonpayload is:");
+            Debug.WriteLine(weapon.WeaponName);
+            //Objective: Add a new weapon into the system using the API 
+            //curl -H "Content-Type:application/json" -d @weapon.json https://localhost:44311/api/weapondata/addweapon
+            string url = "weapondata/addweapon";
+
+            string jsonpayload = jss.Serialize(weapon);
+            Debug.WriteLine(jsonpayload);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+        // GET: Weapon/Edit/5
+        public ActionResult Edit(int id)
+        {
+            UpdateWeapon ViewModel = new UpdateWeapon();
+
+            //the existing Weapon information
+            string url = "weapondata/findweapon/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            WeaponDto SelectedWeapon = response.Content.ReadAsAsync<WeaponDto>().Result;
+            ViewModel.SelectedWeapon = SelectedWeapon;
+
+            // all collections to choose from when updating this animal
+            //the existing weapon information
+            url = "collectionsdata/listcollections/";
+            response = client.GetAsync(url).Result;
+            IEnumerable<CollectionsDto> CollectionsOptions = response.Content.ReadAsAsync<IEnumerable<CollectionsDto>>().Result;
+
+            ViewModel.CollectionsOptions = CollectionsOptions;
+
+            return View(ViewModel);
+        }
+
+        // POST: Weapon/Update/5
+        [HttpPost]
+        public ActionResult Update(int id, Weapon weapon)
+        {
+
+            string url = "weapondata/updateweapon/" + id;
+            string jsonpayload = jss.Serialize(weapon);
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            //Debug.WriteLine(content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        // GET: Weapon/Delete/5
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "weapondata/findweapon/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            WeaponDto selectedweapon = response.Content.ReadAsAsync<WeaponDto>().Result;
+            return View(selectedweapon);
+        }
+
+        // GET: Weapon/Delete/5
+        public ActionResult Delete(int id)
+        {
+            string url = "weapondata/deleteweapon/" + id;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
---
+++ CryptopediaWebApp/Controllers/WeaponDataController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class WeaponDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/WeaponData/ListWeapons
+        [HttpGet]
+        public IEnumerable<WeaponDto> ListWeapons()
+        {
+            List<Weapon> Weapons = db.Weapons.ToList();
+            List<WeaponDto> WeaponDtos = new List<WeaponDto>();
+
+            Weapons.ForEach(a => WeaponDtos.Add(new WeaponDto()
+            {
+                WeaponID = a.WeaponID,
+                WeaponName = a.WeaponName,
+                WeaponCreationYear = a.WeaponCreationYear,
+                WeaponDescription = a.WeaponDescription,
+                CollectionsID = a.Collections.CollectionsID,
+                CollectionsName = a.Collections.CollectionsName,
+                CollectionsStandard = a.Collections.CollectionsStandard
+            }));
+            return WeaponDtos;
+        }
+
+        /// <summary>
+        /// Gathers information about all weapons related to a particular collections ID
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: all weapons in the database, including their associated collections matched with a particular collections ID
+        /// </returns>
+        /// <param name="id">Collections ID.</param>
+        /// <example>
+        /// GET: api/WeaponData/ListWeaponsForCollections/5
+        /// </example>
+        [HttpGet]
+        [ResponseType(typeof(WeaponDto))]
+        public IHttpActionResult ListWeaponsForCollections(int id)
+        {
+            List<Weapon> Weapons = db.Weapons.Where(a => a.CollectionsID == id).ToList();
+            List<WeaponDto> WeaponDtos = new List<WeaponDto>();
+
+            Weapons.ForEach(a => WeaponDtos.Add(new WeaponDto()
+            {
+                WeaponID = a.WeaponID,
+                WeaponName = a.WeaponName,
+                WeaponCreationYear = a.WeaponCreationYear,
+                WeaponDescription = a.WeaponDescription,
+                CollectionsID = a.Collections.CollectionsID,
+                CollectionsName = a.Collections.CollectionsName,
+                CollectionsStandard = a.Collections.CollectionsStandard
+            }));
+
+            return Ok(WeaponDtos);
+        }
+
+        // GET: api/WeaponData/FindWeapon/5
+        [ResponseType(typeof(Weapon))]
+        [HttpGet]
+        public IHttpActionResult FindWeapon(int id)
+        {
+            Weapon Weapon = db.Weapons.Find(id);
+            WeaponDto WeaponDto = new WeaponDto()
+            {
+                WeaponID = Weapon.WeaponID,
+                WeaponName = Weapon.WeaponName,
+                WeaponCreationYear = Weapon.WeaponCreationYear,
+                WeaponDescription = Weapon.WeaponDescription,
+                CollectionsID = Weapon.Collections.CollectionsID,
+                CollectionsName = Weapon.Collections.CollectionsName,
+                CollectionsStandard = Weapon.Collections.CollectionsStandard
+            };
+            if (Weapon == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(WeaponDto);
+        }
+
+        // Post: api/WeaponData/UpdateWeapon/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+
+        public IHttpActionResult UpdateWeapon(int id, Weapon weapon)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != weapon.WeaponID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(weapon).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WeaponExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/WeaponData/AddWeapon
+        [HttpPost]
+        [ResponseType(typeof(Weapon))]
+        public IHttpActionResult AddWeapon(Weapon weapon)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Weapons.Add(weapon);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = weapon.WeaponID }, weapon);
+        }
+
+        /// <summary>
+        /// Deletes an weapon from the system by it's ID.
+        /// </summary>
+        /// <param name="id">The primary key of the weapon</param>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// or
+        /// HEADER: 404 (NOT FOUND)
+        /// </returns>
+        /// <example>
+        /// POST: api/WeaponData/DeleteWeapon/5
+        /// FORM DATA: (empty)
+        /// </example>
+        [ResponseType(typeof(Weapon))]
+        [HttpPost]
+        public IHttpActionResult DeleteWeapon(int id)
+        {
+            Weapon weapon = db.Weapons.Find(id);
+            if (weapon == null)
+            {
+                return NotFound();
+            }
+
+            db.Weapons.Remove(weapon);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool WeaponExists(int id)
+        {
+            return db.Weapons.Count(e => e.WeaponID == id) > 0;
+        }
+    }
+}
